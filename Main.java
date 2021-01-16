50cfef6426d02382049332b15ba7c9b5d4f48b76
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-01-17 01:31:06 +0200

    Update

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         JTM jtm = new JTM();
         try {
             jtm.sendJTM("New Mail Library","The Body of the email"
-                    ,"tawfekyassertawfek@gmail.com","02k0181381t","20180075@stud.fci-cu.edu.eg");
+                    ,"youremail@gmail.com","yourpassword","20180075@stud.fci-cu.edu.eg");
         } catch (MessagingException e) {
             e.printStackTrace();
         }
